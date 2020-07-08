eb414125d94d1c497b18967b22277e4fe836b07c
evsio0n <bbq2001820@gmail.com>
2020-07-08 11:47:06 +0800

    Fix broken type

--- main.go
+++ main.go
@@ -11,6 +11,15 @@ import (
 )
 
 var nullpd string
+var bindAddress string
+var bindPort string
+var bindToAddress string
+var bindToPort string
+var intTest bool = false
+var t int = 0
+var q int = 0
+
+type c2 net.Listener
 
 func readyToCreateThread(readAddress string, readProtocol string) {
 	tcpd := "tcp"
@@ -30,9 +39,7 @@ func readyToCreateThread(readAddress string, readProtocol string) {
 	}
 }
 
-type c net.Listener
-
-func createTcpThread(readAddress string) c {
+func createTcpThread(readAddress string) c2 {
 	c, ers := net.Listen("tcp", readAddress)
 	if ers != nil {
 		log.Error(ers)
@@ -59,17 +66,14 @@ func main() {
 		fmt.Println("err = ", err)
 		return
 	}
-	//关闭文件
 	defer f.Close()
 	r := bufio.NewReader(f)
-	//新建一个缓冲区，把内容先放在缓冲区
 	var check bool = false
 	var buf []byte
 	for {
-		//遇到'\n'结束读取, 但是'\n'也读取进入
 		buf, err = r.ReadBytes('\n')
 		if err != nil {
-			if err == io.EOF { //文件已经结束
+			if err == io.EOF {
 				break
 			}
 			log.Error(err)
@@ -83,8 +87,56 @@ func main() {
 		} else {
 			if strings.Contains(string(buf), "#") {
 			} else {
-				fmt.Print(string(buf))
+				readString := string(buf)
+				readIt(readString)
 			}
 		}
+	}
+}
+
+func readIt(readString string) {
+	i := len(readString)
+	for c := 0; c == i-1; c++ {
+		if string(readString[c]) == " " {
+			t++
+			q++
+			//TODO: Do some thing to read the port.
+			if string(readString[c]) == string(readString[c-1]) {
+				t--
+				//Some people didn't see the fucking instruction, so we should delete deprecated space
+			}
+		}
+		switch t {
+		case 1:
+			//Read bind address
+			bindAddress = string(readString[q:c])
+			log.Info(bindAddress)
+			q = c
+		case 2:
+			//Read bind port
+			bindPort = string(readString[q:c])
+			log.Info(bindPort)
+			q = c
+		case 3:
+			//Read bind to address
+			bindToAddress = string(readString[q:c])
+			log.Info(bindToAddress)
+			q = c
+		case 4:
+			//Read bind to port
+			bindToPort = string(readString[q:c])
+			log.Info(bindToPort)
+			q = c
+
+		}
+
+	}
+}
+
+func checkIPAddressType(ip string) bool {
+	if net.ParseIP(ip) == nil {
+		return false
+	} else {
+		return true
 	}
 }
